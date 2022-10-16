ded2b494d73c44051c3d25652a42519a496ba153
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-16 11:49:50 +0300

    register servers

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/hashicorp/go-hclog"
+	gService "github.com/oleksiivelychko/go-grpc-protobuf/proto/grpc_service"
+	"github.com/oleksiivelychko/go-grpc-protobuf/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
+)
+
+func main() {
+	logger := hclog.Default()
+	gServer := grpc.NewServer()
+	reflection.Register(gServer)
+	sServer := server.NewServer(logger)
+	gService.RegisterProductServer(gServer, sServer)
+
+	listen, err := net.Listen("tcp", "localhost:9090")
+	if err != nil {
+		logger.Error("Unable to listen", "error", err)
+		os.Exit(1)
+	}
+
+	_ = gServer.Serve(listen)
+}
