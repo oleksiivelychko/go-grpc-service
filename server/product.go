11cf77287d9fbdf8b00c69c4d35def1274c9745c
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-22 14:48:00 +0300

    create product server

---
+++ server/product.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"context"
+	"github.com/hashicorp/go-hclog"
+	gService "github.com/oleksiivelychko/go-grpc-protobuf/proto/grpc_service"
+)
+
+type ProductServer struct {
+	log hclog.Logger
+}
+
+func NewProductServer(l hclog.Logger) *ProductServer {
+	return &ProductServer{l}
+}
+
+/*
+GetProduct returns the sample product.
+*/
+func (s *ProductServer) GetProduct(_ context.Context, r *gService.ProductRequest) (*gService.ProductResponse, error) {
+	s.log.Info("[INFO] handle `grpc_service.Product.GetProduct`", "ID", r.GetId())
+
+	department := gService.ProductResponse_Department{
+		Number: "D1",
+		Type:   gService.ProductResponse_MarketType(2),
+	}
+
+	return &gService.ProductResponse{
+		Name:        "Main product",
+		Sku:         "000-000-000",
+		Price:       0.99,
+		Departments: []*gService.ProductResponse_Department{&department},
+	}, nil
+}
+
+/*
+*
+Requires to compile without 'require_unimplemented_servers'
+*/
+func (s *ProductServer) mustEmbedUnimplementedProductServer() {
+	s.log.Info("implement mustEmbedUnimplementedProductServer for backward compatibility")
+}
