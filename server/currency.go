67fe3d3063a7fa46d6279350f1941652a82ff4fd
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-23 07:48:39 +0300

    create currency server

---
+++ server/currency.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"context"
+	"github.com/hashicorp/go-hclog"
+	"github.com/oleksiivelychko/go-grpc-protobuf/data"
+	gService "github.com/oleksiivelychko/go-grpc-protobuf/proto/grpc_service"
+)
+
+type CurrencyServer struct {
+	log hclog.Logger
+	e   *data.Exchanger
+}
+
+func NewCurrencyServer(l hclog.Logger, e *data.Exchanger) *CurrencyServer {
+	return &CurrencyServer{l, e}
+}
+
+func (cs *CurrencyServer) MakeExchange(_ context.Context, r *gService.ExchangeRequest) (*gService.ExchangeResponse, error) {
+	cs.log.Info("[INFO] handle `grpc_service.Currency.MakeExchange`", "from", r.GetFrom(), "to", r.GetTo())
+
+	rate, err := cs.e.GetRate(r.GetFrom(), r.GetTo())
+	if err != nil {
+		cs.log.Info("[ERROR] cannot get rate", "error", err)
+	}
+
+	return &gService.ExchangeResponse{
+		Rate: rate,
+	}, nil
+}
