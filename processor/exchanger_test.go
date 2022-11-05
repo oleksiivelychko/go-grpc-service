b09097be30d3efa6f90791d41c3b0fa9e665f296
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-11-05 08:05:28 +0200

    inject exchanger by extractor

--- processor/exchanger_test.go
+++ processor/exchanger_test.go
@@ -2,15 +2,17 @@ package processor
 
 import (
 	"fmt"
+	"github.com/oleksiivelychko/go-grpc-protobuf/data"
 	"testing"
 )
 
 func TestNewExchanger(t *testing.T) {
-	e, err := NewExchanger()
+	extractor := data.NewExtractorXml(data.SourceLocal)
+	e, err := NewExchanger(extractor)
 
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	fmt.Printf("Rates: %#v", e.rates)
+	fmt.Printf("Rates: %#v\n", e.rates)
 }
