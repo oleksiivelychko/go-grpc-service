836583bb916faae92caa4180f22d032316aa8118
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-16 08:28:49 +0200

    refactoring currency server

--- server/currency.go
+++ server/currency.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/hashicorp/go-hclog"
 	"github.com/oleksiivelychko/go-grpc-service/processor"
-	gService "github.com/oleksiivelychko/go-grpc-service/proto/grpc_service"
+	grpcService "github.com/oleksiivelychko/go-grpc-service/proto/grpc_service"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"io"
@@ -14,64 +14,67 @@ import (
 type CurrencyServer struct {
 	logger            hclog.Logger
 	exchanger         *processor.Exchanger
-	subscribedClients map[gService.Currency_SubscriberServer][]*gService.ExchangeRequest
+	subscribedClients map[grpcService.Currency_SubscriberServer][]*grpcService.ExchangeRequest
 }
 
-func NewCurrencyServer(l hclog.Logger, e *processor.Exchanger) *CurrencyServer {
-	requests := make(map[gService.Currency_SubscriberServer][]*gService.ExchangeRequest)
-	cServer := &CurrencyServer{l, e, requests}
+func NewCurrencyServer(logger hclog.Logger, exchanger *processor.Exchanger) *CurrencyServer {
+	exchangeRequests := make(map[grpcService.Currency_SubscriberServer][]*grpcService.ExchangeRequest)
 
-	go cServer.handleUpdates()
+	currencyServer := &CurrencyServer{logger, exchanger, exchangeRequests}
 
-	return cServer
+	go currencyServer.handleUpdates()
+	return currencyServer
 }
 
-func (cs *CurrencyServer) handleUpdates() {
-	updates := cs.exchanger.TrackRates(5 * time.Second)
+func (currencyServer *CurrencyServer) handleUpdates() {
+	updates := currencyServer.exchanger.TrackRates(5 * time.Second)
 	for range updates {
-		cs.logger.Info("handling updates...")
+		currencyServer.logger.Info("handling updates...")
 
-		for subscribedClient, exchangeRequests := range cs.subscribedClients {
+		for subscribedClient, exchangeRequests := range currencyServer.subscribedClients {
 			for _, exchangeRequest := range exchangeRequests {
 				fromCurrency := exchangeRequest.GetFrom().String()
 				toCurrency := exchangeRequest.GetTo().String()
 
-				rate, err := cs.exchanger.GetRate(fromCurrency, toCurrency)
+				rate, err := currencyServer.exchanger.GetRate(fromCurrency, toCurrency)
 				if err != nil {
-					cs.logger.Error("unable to get update of rate", "from", fromCurrency, "to", toCurrency)
+					currencyServer.logger.Error("unable to get update of rate", "from", fromCurrency, "to", toCurrency)
 				}
 
-				err = subscribedClient.Send(&gService.StreamExchangeResponse{
-					Message: &gService.StreamExchangeResponse_ExchangeResponse{
-						ExchangeResponse: &gService.ExchangeResponse{
+				err = subscribedClient.Send(&grpcService.StreamExchangeResponse{
+					Message: &grpcService.StreamExchangeResponse_ExchangeResponse{
+						ExchangeResponse: &grpcService.ExchangeResponse{
 							From:      exchangeRequest.GetFrom(),
 							To:        exchangeRequest.GetTo(),
 							Rate:      rate,
-							CreatedAt: cs.exchanger.GetProtoTime(),
+							CreatedAt: currencyServer.exchanger.GetProtoTimestamp(),
 						},
 					},
 				})
 
 				if err != nil {
-					cs.logger.Error("unable to send updated rate", "from", fromCurrency, "to", toCurrency, "rate", rate)
+					currencyServer.logger.Error("unable to send updated rate", "from", fromCurrency, "to", toCurrency, "rate", rate)
 				}
 			}
 		}
 	}
 }
 
-func (cs *CurrencyServer) MakeExchange(_ context.Context, r *gService.ExchangeRequest) (*gService.ExchangeResponse, error) {
-	cs.logger.Info("handle `grpc_service.Currency.MakeExchange`", "from", r.GetFrom(), "to", r.GetTo())
+func (currencyServer *CurrencyServer) MakeExchange(
+	_ context.Context,
+	exchangeRequest *grpcService.ExchangeRequest,
+) (*grpcService.ExchangeResponse, error) {
+	currencyServer.logger.Info("handle `grpc_service.Currency.MakeExchange`", "from", exchangeRequest.GetFrom(), "to", exchangeRequest.GetTo())
 
-	if r.GetFrom() == r.GetTo() {
+	if exchangeRequest.GetFrom() == exchangeRequest.GetTo() {
 		grpcErr := status.Newf(
 			codes.InvalidArgument,
 			"base currency '%s' cannot be the same as destination '%s'",
-			r.GetFrom(),
-			r.GetTo(),
+			exchangeRequest.GetFrom(),
+			exchangeRequest.GetTo(),
 		)
 
-		grpcStatus, err := grpcErr.WithDetails(r)
+		grpcStatus, err := grpcErr.WithDetails(exchangeRequest)
 		if err != nil {
 			return nil, err
 		}
@@ -79,42 +82,42 @@ func (cs *CurrencyServer) MakeExchange(_ context.Context, r *gService.ExchangeRe
 		return nil, grpcStatus.Err()
 	}
 
-	rate, err := cs.exchanger.GetRate(r.GetFrom().String(), r.GetTo().String())
+	rate, err := currencyServer.exchanger.GetRate(exchangeRequest.GetFrom().String(), exchangeRequest.GetTo().String())
 	if err != nil {
-		cs.logger.Error("cannot get rate", "error", err)
+		currencyServer.logger.Error("cannot get rate", "error", err)
 	}
 
-	return &gService.ExchangeResponse{
+	return &grpcService.ExchangeResponse{
 		Rate:      rate,
-		From:      r.GetFrom(),
-		To:        r.GetTo(),
-		CreatedAt: cs.exchanger.GetProtoTime(),
+		From:      exchangeRequest.GetFrom(),
+		To:        exchangeRequest.GetTo(),
+		CreatedAt: currencyServer.exchanger.GetProtoTimestamp(),
 	}, nil
 }
 
 /*
 Subscriber implements the gRPC bidirectional streaming method.
 */
-func (cs *CurrencyServer) Subscriber(srv gService.Currency_SubscriberServer) error {
+func (currencyServer *CurrencyServer) Subscriber(subscriberServer grpcService.Currency_SubscriberServer) error {
 	// handle client messages
 	for {
 		// 'Recv' is a blocking method which returns on client data.
-		exchangeRequest, err := srv.Recv()
+		exchangeRequest, err := subscriberServer.Recv()
 		if err == io.EOF {
-			cs.logger.Error("client has closed the connection")
+			currencyServer.logger.Error("client has closed the connection")
 			break
 		}
 
 		if err != nil {
-			cs.logger.Error("unable to read from client", "error", err)
+			currencyServer.logger.Error("unable to read from client", "error", err)
 			return err
 		}
 
-		cs.logger.Info("handle client request", "From", exchangeRequest.GetFrom(), "To", exchangeRequest.GetTo())
+		currencyServer.logger.Info("handle client request", "From", exchangeRequest.GetFrom(), "To", exchangeRequest.GetTo())
 
-		subscribedClient, ok := cs.subscribedClients[srv]
+		subscribedClient, ok := currencyServer.subscribedClients[subscriberServer]
 		if !ok {
-			subscribedClient = []*gService.ExchangeRequest{}
+			subscribedClient = []*grpcService.ExchangeRequest{}
 		}
 
 		var validationErr *status.Status
@@ -130,7 +133,7 @@ func (cs *CurrencyServer) Subscriber(srv gService.Currency_SubscriberServer) err
 				)
 
 				if validationErr, err = validationErr.WithDetails(exchangeRequest); err != nil {
-					cs.logger.Error("unable to original request as metadata", "error", err)
+					currencyServer.logger.Error("unable to original request as metadata", "error", err)
 				}
 
 				break
@@ -138,21 +141,21 @@ func (cs *CurrencyServer) Subscriber(srv gService.Currency_SubscriberServer) err
 		}
 
 		if validationErr != nil {
-			err = srv.Send(&gService.StreamExchangeResponse{
-				Message: &gService.StreamExchangeResponse_Error{
+			err = subscriberServer.Send(&grpcService.StreamExchangeResponse{
+				Message: &grpcService.StreamExchangeResponse_Error{
 					Error: validationErr.Proto(),
 				},
 			})
 
 			if err != nil {
-				cs.logger.Error("unable to send validation message", "error", err)
+				currencyServer.logger.Error("unable to send validation message", "error", err)
 			}
 
 			continue
 		}
 
 		subscribedClient = append(subscribedClient, exchangeRequest)
-		cs.subscribedClients[srv] = subscribedClient
+		currencyServer.subscribedClients[subscriberServer] = subscribedClient
 	}
 
 	return nil
@@ -162,6 +165,6 @@ func (cs *CurrencyServer) Subscriber(srv gService.Currency_SubscriberServer) err
 *
 mustEmbedUnimplementedCurrencyServer is required to compile without 'require_unimplemented_servers'.
 */
-func (cs *CurrencyServer) mustEmbedUnimplementedCurrencyServer() {
-	cs.logger.Info("implement mustEmbedUnimplementedCurrencyServer for backward compatibility")
+func (currencyServer *CurrencyServer) mustEmbedUnimplementedCurrencyServer() {
+	currencyServer.logger.Info("implement mustEmbedUnimplementedCurrencyServer for backward compatibility")
 }
