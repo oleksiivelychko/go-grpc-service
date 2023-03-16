e6e0ddcb332f5a82e437379b56f83956b1592039
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-16 08:26:31 +0200

    refactoring exchanger

--- processor/exchanger.go
+++ processor/exchanger.go
@@ -2,8 +2,8 @@ package processor
 
 import (
 	"fmt"
-	"github.com/oleksiivelychko/go-grpc-service/data"
 	"github.com/oleksiivelychko/go-grpc-service/proto/grpc_service"
+	extractor "github.com/oleksiivelychko/go-grpc-service/xml_extractor"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"math/rand"
 	"strconv"
@@ -11,23 +11,23 @@ import (
 )
 
 type Exchanger struct {
-	rates     map[string]float64
-	extractor *data.Extractor
+	rates        map[string]float64
+	xmlExtractor *extractor.XmlExtractor
 }
 
-func NewExchanger(e *data.Extractor) (*Exchanger, error) {
-	exchanger := &Exchanger{extractor: e, rates: map[string]float64{}}
+func NewExchanger(xmlExtractor *extractor.XmlExtractor) (*Exchanger, error) {
+	exchanger := &Exchanger{xmlExtractor: xmlExtractor, rates: map[string]float64{}}
 	err := exchanger.processRates()
 	return exchanger, err
 }
 
-func (e *Exchanger) GetRate(fromCurrency, toCurrency string) (float64, error) {
-	rateFromCurrency, ok := e.rates[fromCurrency]
+func (exchanger *Exchanger) GetRate(fromCurrency, toCurrency string) (float64, error) {
+	rateFromCurrency, ok := exchanger.rates[fromCurrency]
 	if !ok {
 		return 0, fmt.Errorf("rate not found for base [from] currency %s", fromCurrency)
 	}
 
-	rateToCurrency, ok := e.rates[toCurrency]
+	rateToCurrency, ok := exchanger.rates[toCurrency]
 	if !ok {
 		return 0, fmt.Errorf("rate not found for destination [to] currency %s", toCurrency)
 	}
@@ -35,22 +35,22 @@ func (e *Exchanger) GetRate(fromCurrency, toCurrency string) (float64, error) {
 	return rateFromCurrency / rateToCurrency, nil
 }
 
-func (e *Exchanger) processRates() error {
-	err := e.extractor.FetchRates()
+func (exchanger *Exchanger) processRates() error {
+	err := exchanger.xmlExtractor.FetchData()
 	if err != nil {
 		return err
 	}
 
-	for _, cube := range e.extractor.RootNode.Data.Cubes {
+	for _, cube := range exchanger.xmlExtractor.RootNode.Data.Cubes {
 		rate, parseErr := strconv.ParseFloat(cube.Rate, 64)
 		if parseErr != nil {
 			return fmt.Errorf("cannot parse the rate value `%s` to float. %s", cube.Rate, parseErr)
 		}
 
-		e.rates[cube.Currency] = rate
+		exchanger.rates[cube.Currency] = rate
 	}
 
-	e.rates[grpc_service.Currencies_EUR.String()] = 1
+	exchanger.rates[grpc_service.Currencies_EUR.String()] = 1
 
 	return nil
 }
@@ -59,7 +59,7 @@ func (e *Exchanger) processRates() error {
 TrackRates sends message to the channel when rates are changed.
 Fake simulation process, the local development use only.
 */
-func (e *Exchanger) TrackRates(interval time.Duration) chan struct{} {
+func (exchanger *Exchanger) TrackRates(interval time.Duration) chan struct{} {
 	ch := make(chan struct{})
 
 	go func() {
@@ -67,7 +67,7 @@ func (e *Exchanger) TrackRates(interval time.Duration) chan struct{} {
 		for {
 			select {
 			case <-ticker.C:
-				for currency, rate := range e.rates {
+				for currency, rate := range exchanger.rates {
 					// can be 10% of original value
 					change := rand.Float64() / 10
 					isPositive := rand.Intn(1)
@@ -79,7 +79,7 @@ func (e *Exchanger) TrackRates(interval time.Duration) chan struct{} {
 						change = 1 + change
 					}
 
-					e.rates[currency] = rate * change
+					exchanger.rates[currency] = rate * change
 				}
 
 				// notify updates, this will block unless there is a listener on the other end
@@ -91,8 +91,8 @@ func (e *Exchanger) TrackRates(interval time.Duration) chan struct{} {
 	return ch
 }
 
-func (e *Exchanger) GetProtoTime() *timestamppb.Timestamp {
-	createdAt, err := time.Parse("2006-01-02", e.extractor.RootNode.Data.Time)
+func (exchanger *Exchanger) GetProtoTimestamp() *timestamppb.Timestamp {
+	createdAt, err := time.Parse("2006-01-02", exchanger.xmlExtractor.RootNode.Data.Time)
 	if err != nil {
 		createdAt = timestamppb.Now().AsTime()
 	}
--- processor/exchanger_test.go
+++ processor/exchanger_test.go
@@ -2,17 +2,22 @@ package processor
 
 import (
 	"fmt"
-	"github.com/oleksiivelychko/go-grpc-service/data"
+	extractor "github.com/oleksiivelychko/go-grpc-service/xml_extractor"
 	"testing"
 )
 
 func TestNewExchanger(t *testing.T) {
-	extractor := data.NewExtractor(data.SourceLocal)
-	e, err := NewExchanger(extractor)
+	xmlExtractor := extractor.NewXmlExtractor(extractor.SourceLocal)
+	e, err := NewExchanger(xmlExtractor)
 
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
 	fmt.Printf("Rates: %#v\n", e.rates)
+
+	err = xmlExtractor.RemoveFile()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
 }
