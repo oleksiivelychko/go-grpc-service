c3c7cffaf21d228bb4f250d2fad35b297221273a
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-19 12:16:07 +0200

    rewrite tests

--- extractor_xml/extractor_xml_test.go
+++ extractor_xml/extractor_xml_test.go
@@ -1,39 +1,17 @@
 package extractor_xml
 
 import (
+	"fmt"
+	"github.com/oleksiivelychko/go-utils/file_ops"
 	"strconv"
 	"testing"
 )
 
-func TestExtractorXML_FetchDataFromURL(t *testing.T) {
-	extractorXML := NewExtractorXML(SourceURL)
-
-	err := extractorXML.FetchData()
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	if extractorXML.source != SourceURL {
-		t.Fatal("source doesn't equal to SourceURL")
-	}
-
-	if extractorXML.RootNode.Data.Time == "" {
-		t.Fatal("attribute `time` couldn't extracted from `Cube` element")
-	}
-
-	for _, cube := range extractorXML.RootNode.Data.Cubes {
-		_, parseFloatErr := strconv.ParseFloat(cube.Rate, 64)
-		if parseFloatErr != nil {
-			t.Error(parseFloatErr)
-		}
-	}
-}
-
 func TestExtractorXML_FetchDataFromLocalFirstTime(t *testing.T) {
 	extractorXML := NewExtractorXML(SourceLocal)
 
-	if extractorXML.isExistFile() {
-		err := extractorXML.RemoveFile()
+	if file_ops.DoesFileExist(localXML) {
+		err := file_ops.DeleteFile(localXML)
 		if err != nil {
 			t.Fatalf(err.Error())
 		}
@@ -48,47 +26,58 @@ func TestExtractorXML_FetchDataFromLocalFirstTime(t *testing.T) {
 		t.Fatal("source doesn't equal to SourceURL")
 	}
 
+	testData(extractorXML, t)
+}
+
+func TestExtractorXML_FetchDataFromLocal(t *testing.T) {
+	if !file_ops.DoesFileExist(localXML) {
+		t.Fatalf("file %s doesn't exist", localXML)
+	}
+
+	extractorXML := NewExtractorXML(SourceLocal)
+
+	err := extractorXML.FetchData()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if extractorXML.source != SourceLocal {
+		t.Fatal("source doesn't equal to SourceURL")
+	}
+
+	testData(extractorXML, t)
+
+	err = file_ops.DeleteFile(localXML)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestExtractorXML_FetchDataFromURL(t *testing.T) {
+	extractorXML := NewExtractorXML(SourceURL)
+
+	err := extractorXML.FetchData()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if extractorXML.source != SourceURL {
+		t.Fatal("source doesn't equal to SourceURL")
+	}
+
+	testData(extractorXML, t)
+}
+
+func testData(extractorXML *ExtractorXML, t *testing.T) {
 	if extractorXML.RootNode.Data.Time == "" {
 		t.Fatal("attribute `time` couldn't extracted from `Cube` element")
 	}
 
 	for _, cube := range extractorXML.RootNode.Data.Cubes {
-		_, parseFloatErr := strconv.ParseFloat(cube.Rate, 64)
+		rate, parseFloatErr := strconv.ParseFloat(cube.Rate, 64)
 		if parseFloatErr != nil {
 			t.Error(parseFloatErr)
 		}
-	}
-}
-
-func TestExtractorXML_FetchDataFromLocal(t *testing.T) {
-	xmlExtractor := NewExtractorXML(SourceLocal)
-
-	err := xmlExtractor.FetchData()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	if !xmlExtractor.isExistFile() {
-		t.Fatalf("local file `%s` doesn't exist", localXML)
-	}
-
-	if xmlExtractor.source != SourceLocal {
-		t.Fatal("source doesn't equal to SourceURL")
-	}
-
-	if xmlExtractor.RootNode.Data.Time == "" {
-		t.Fatal("attribute `time` couldn't extracted from `Cube` element")
-	}
-
-	for _, cube := range xmlExtractor.RootNode.Data.Cubes {
-		_, parseFloatErr := strconv.ParseFloat(cube.Rate, 64)
-		if parseFloatErr != nil {
-			t.Error(parseFloatErr)
-		}
-	}
-
-	err = xmlExtractor.RemoveFile()
-	if err != nil {
-		t.Fatalf(err.Error())
+		fmt.Printf("currency: %s, rate: %f\n", cube.Currency, rate)
 	}
 }
