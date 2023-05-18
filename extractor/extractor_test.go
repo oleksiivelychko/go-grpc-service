9f5374364ebebdb3699ee77b39853e7b2105578a
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-05-18 09:21:39 +0300

    mock data from url test case

--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -1,7 +1,11 @@
 package extractor
 
 import (
+	"encoding/xml"
 	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"strconv"
 	"testing"
@@ -26,6 +30,53 @@ func TestExtractor_TryToExtractDataFromLocalXML(t *testing.T) {
 
 	if extractor.source != SourceURL {
 		t.Fatal("source is not equal to SourceURL")
+	}
+
+	testData(extractor, t)
+}
+
+func TestExtractor_MockDataFromURL(t *testing.T) {
+	if _, err := os.Stat(localXML); errors.Is(err, os.ErrNotExist) {
+		t.Fatal(err)
+	}
+
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		data, err := os.ReadFile(localXML)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, _ = io.WriteString(resp, string(data))
+	}
+
+	req := httptest.NewRequest("GET", "https://example.com", nil)
+	resp := httptest.NewRecorder()
+
+	resp.Header().Set("Content-Type", "application/xml")
+	handler(resp, req)
+
+	response := resp.Result()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("got %d status code", response.StatusCode)
+	}
+
+	if response.Header.Get("Content-Type") != "application/xml" {
+		t.Errorf("content type mismatch: %s", response.Header.Get("Content-Type"))
+	}
+
+	if len(string(body)) == 0 {
+		t.Error("unable to fetch data")
+	}
+
+	extractor := New(SourceURL, localXML)
+	err = xml.Unmarshal(body, &extractor.RootNode)
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
 
 	testData(extractor, t)
